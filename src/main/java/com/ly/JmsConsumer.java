7bf0c9df8cd35fa3bebbdf2835c64605af08a68e
kalipy <3069087972@qq.com>
2020-11-25 23:57:59 +0800

    异步监听方式接收消息

--- src/main/java/com/ly/JmsConsumer.java
+++ src/main/java/com/ly/JmsConsumer.java
@@ -1,8 +1,12 @@
 package com.ly;
 
+import java.io.IOException;
+
 import javax.jms.Connection;
 import javax.jms.JMSException;
+import javax.jms.Message;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.TextMessage;
@@ -20,13 +24,14 @@ public class JmsConsumer
     public static final String ACTIVEMQ_URL = "tcp://127.0.0.1:61616";
     public static final String QUEUE_NAME = "queue01";
 
-    public static void main(String[] args) throws JMSException {
+    public static void main(String[] args) throws JMSException, IOException, Exception  {
         ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
         Connection connection = activeMQConnectionFactory.createConnection();
         connection.start();
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         Queue queue = session.createQueue(QUEUE_NAME);
         MessageConsumer messageConsumer = session.createConsumer(queue);
+        /*
         //同步阻塞方式
         //订阅者或接受者调用MessageConsumer的receive()来接收消息，receive在接收到消息之前或超时之前将一直阻塞
         while (true) {
@@ -38,6 +43,25 @@ public class JmsConsumer
                 break;
             }
         }
+        messageConsumer.close();
+        session.close();
+        connection.close();*/
+
+        //监听方式 
+        messageConsumer.setMessageListener(new MessageListener() {
+            public void onMessage(Message message) {
+                if (null != message && message instanceof TextMessage) {
+                    TextMessage textMessage = (TextMessage) message;
+                    try {
+                        System.out.println("消费者接收到消息："+textMessage.getText());
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        //System.in.read();//在gradle中无效,等待一会，因为监听需要时间，不然还没监听到connection就已经断开了
+        Thread.sleep(3*1000);//用sleep代替read()
         messageConsumer.close();
         session.close();
         connection.close();
